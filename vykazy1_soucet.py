732125a685d8e9315cbdc12a9472fcd968de2a6b
merkuloi <ivan.merkulov188@student.cuni.cz>
2025-11-03 10:26:19 +0100

    Hodina 3.11.2025 - pridani komentaru do vykazy1_soucet.py

--- vykazy1_soucet.py
+++ vykazy1_soucet.py
@@ -1,3 +1,4 @@
+# Zkontroluje zdali je radek validni
 def is_valid(line) -> bool:
     if len(line) < 5:
         return False
@@ -5,11 +6,13 @@ def is_valid(line) -> bool:
         return False
     return True
 
+# Prevede cas ve formatu H:mm:ss do sekund
 def time2sec(time) -> int:
     time = time.split(":")
     sec_time = int(time[2]) + int(time[1])*60 + int(time[0])*3600
     return sec_time
 
+# Prevede celkovy cas v sekundach na format H:mm:ss
 def sec2time(time):
     seconds = time % 60
     if seconds < 10:
@@ -24,10 +27,10 @@ def sec2time(time):
     result = str(hours) + ":" + str(minutes) + ":" + str(seconds)
     return result
 
-
 total_time = 0
 current_line = input().split()
 
+# Nacte radku ze vstupu a rovnou zkontroluje jestli je validni a pokud ano pripocita cas
 while current_line != ["."]:
     if is_valid(current_line):
         total_time += time2sec(current_line[-1]) - time2sec(current_line[1])
