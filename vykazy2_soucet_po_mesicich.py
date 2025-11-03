f242ce0fefea252747f2207ff80ee05338fc782c
merkuloi <ivan.merkulov188@student.cuni.cz>
2025-11-03 11:17:06 +0100

    Hodina 3.11.2025 - vykazy2_soucet_po_mesicich.py

---
+++ vykazy2_soucet_po_mesicich.py
@@ -0,0 +1,53 @@
+# Zkontroluje zdali je radek validni
+def is_valid(line) -> bool:
+    if len(line) < 5:
+        return False
+    if line[0] != line[-2]:
+        return False
+    return True
+
+# Prevede cas ve formatu H:mm:ss do sekund
+def time2sec(time) -> int:
+    time = time.split(":")
+    sec_time = int(time[2]) + int(time[1])*60 + int(time[0])*3600
+    return sec_time
+
+# Prevede celkovy cas v sekundach na format H:mm:ss
+def sec2time(time):
+    seconds = time % 60
+    if seconds < 10:
+        seconds = "0" + str(seconds)
+    time = time // 60
+    minutes = time % 60
+    if minutes < 10:
+        minutes = "0" + str(minutes)
+    time = time // 60
+    hours = time
+
+    result = str(hours) + ":" + str(minutes) + ":" + str(seconds)
+    return result
+
+total_time = 0
+current_line = input().split()
+cas_za_mesic = 0
+seznam_mesicu = []
+
+# Nacte radku ze vstupu a rovnou zkontroluje jestli je validni a pokud ano pripocita cas
+while current_line != ["."]:
+    if is_valid(current_line):
+        total_time += time2sec(current_line[-1]) - time2sec(current_line[1])
+        seznam_mesicu.append(current_line)
+    current_line = input().split()
+
+mesic = seznam_mesicu[0][0].split(".")[1]
+rok = seznam_mesicu[0][0].split(".")[2]
+for i in range(len(seznam_mesicu)):
+    if mesic == seznam_mesicu[i][0].split(".")[1]:
+        cas_za_mesic += time2sec(seznam_mesicu[i][-1]) - time2sec(seznam_mesicu[i][1])
+    else:
+        print(f"{mesic}/{rok}: {sec2time(cas_za_mesic)}")
+        mesic = seznam_mesicu[i][0].split(".")[1]
+        rok = seznam_mesicu[i][0].split(".")[2]
+        cas_za_mesic = time2sec(seznam_mesicu[i][-1]) - time2sec(seznam_mesicu[i][1])
+print(f"{mesic}/{rok}: {sec2time(cas_za_mesic)}")
+print("celkem:",sec2time(total_time))
